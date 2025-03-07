ed36fedcf372b579c14c3a8a0336ccbc42218f1e
Langtian Lang <langtian@maccheese.local>
2025-03-06 21:05:15 -0500

    functions of cols and canvas

--- src/types/CalendarTypes.ts
+++ src/types/CalendarTypes.ts
@@ -1,6 +1,7 @@
 import { CalendarBox } from "./CalendarBox";
 import { getCurrentWeekBoxes } from "../utils/AffairUtils";
 import { isSameDay } from "../utils/AffairUtils";
+import { getCurrentWeekDates } from "../utils/DateUtils";
 
 export class CalendarColumn {
     public id: string = "calendar-column-";
@@ -8,10 +9,6 @@ export class CalendarColumn {
     constructor(
         public date: Date,
     ) {}
-
-    getBoxes(): CalendarBox[] {
-        return this.boxes;
-    }
 };
 
 export class CalendarCanvas {
@@ -35,7 +32,7 @@ export class CalendarCanvas {
 
         this.boxes = getCurrentWeekBoxes(this.weekDays);
 
-        
+        this.assignColumns();
 
         return true;
     }
@@ -50,13 +47,22 @@ export class CalendarCanvas {
         this.boxes = getCurrentWeekBoxes(this.weekDays);
 
         return true;
-    }
+    } 
 
-    private getBoxesOfDate(date: Date): CalendarBox[] {
-        return this.boxes.filter(box => isSameDay(box.date, date));
-    }
-
-    assignColumns() {
-
+    private assignColumns(): void {
+        if (this.columns.length === 0) {
+            for (const date of this.weekDays) {
+                const col = new CalendarColumn(date);
+                col.boxes = this.boxes.filter(b => isSameDay(b.date, date));
+                col.id += date.getDay();
+            }
+        } else if (this.columns.length === 7){
+            let index = 0;
+            for (const date of this.weekDays) {
+                this.columns[index++].boxes = this.boxes.filter(b => isSameDay(b.date, date));
+            }
+        } else {
+            throw new Error(`Internal error: invalid column length: ${this.columns.length}`);
+        }
     }
 };
--- src/utils/CalendarUtils.ts
+++ src/utils/CalendarUtils.ts
@@ -1,3 +1,6 @@
 import { CalendarBox } from "../types/CalendarBox";
 import { CalendarColumn, CalendarCanvas } from "../types/CalendarTypes";
 
+export function getCurrentDateBoxes(date: Date, canvas: CalendarCanvas): CalendarBox[] {
+    return canvas.boxes.filter(b => b.date === date);
+}
