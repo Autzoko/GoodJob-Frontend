5156074a1c67f068d50b19c12078d50e2b486e7e
Langtian Lang <langtian@maccheese.local>
2025-03-13 23:51:53 -0400

    can edit and delete, still many bugs

--- src/components/Calendar/ColumnBoxUnit.tsx
+++ src/components/Calendar/ColumnBoxUnit.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { CalendarBox } from "../../types/CalendarBox";
 import { getBoxColor } from "../../utils/BoxUtils";
+import UpdateAffair from "../Dialog/UpdateAffairDialog/UpdateAffair";
 
 interface Props {
     box: CalendarBox;
@@ -8,21 +9,32 @@ interface Props {
 
 const ColumnBoxUnit: React.FC<Props> = ({ box }) => {
     const color = getBoxColor(box);
+    const [updateDialogOpen, setUpdateDialogOpen] = useState<boolean>(false);
+
+    const handleClick = () => {
+        setUpdateDialogOpen(true);
+    };
+
     return (
-        <div
-            style={{
-                backgroundColor: color,
-                margin: '4px 0',
-                padding: '4px',
-                borderRadius: '4px',
-                overflow: 'hidden',
-                textOverflow: 'ellipsis',
-                whiteSpace: 'nowrap'
-            }}
-            title={`${box.title} (${box.startTime} - ${box.endTime})`}
-        >
-            {box.title} ({box.startTime} - {box.endTime})
-        </div>
+        <>
+            <div
+                style={{
+                    backgroundColor: color,
+                    margin: '4px 0',
+                    padding: '4px',
+                    borderRadius: '4px',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    whiteSpace: 'nowrap'
+                }}
+                title={`${box.title} (${box.startTime} - ${box.endTime})`}
+                onClick={handleClick}
+            >
+                {box.title} ({box.startTime} - {box.endTime})
+            </div>
+
+            <UpdateAffair open={updateDialogOpen} id={box.id} onClose={() => setUpdateDialogOpen(false)} />
+        </>
     );
 };
 
